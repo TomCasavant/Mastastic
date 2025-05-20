b223e29bc0a48bfedb923c3ef027dfc5a825b66d
Tom Casavant <tfcasavant@gmail.com>
2025-05-20 12:22:32 -0400

    Fixed notifications and help functions

--- client.py
+++ client.py
@@ -40,6 +40,7 @@ class MastodonClient:
     def post_status(self, status):
         try:
             if self.mastodon:
+                # TODO: Replace w/ the mastodon post method so we have finer control over visibility?
                 self.mastodon.toot(status)
                 print("Posted to Mastodon: ", status)
             else:
@@ -47,24 +48,37 @@ class MastodonClient:
         except Exception as e:
             print(f"Error posting status: {e}")
 
-
 class NotificationListener(StreamListener):
     """Listen to Mastodon notifications."""
     def __init__(self, interface):
         self.interface = interface
 
+    def clean_text(self, text):
+        # Remove HTML tags and decode HTML entities
+        tree = fromstring(text)
+        text_content = tree.text_content()
+        return text_content
+
+    def build_notification(self, notification):
+        text_notification = f"{notification.account.acct}"
+        match notification.type:
+            case 'mention':
+                text = self.clean_text(notification.status.content)
+                text_notification += f" mentioned you: {text}"
+            case 'favourite':
+                text_notification += " liked your status"
+            case 'reblog':
+                text_notification += " boosted your status"
+            case 'follow':
+                text_notification += " followed you"
+            case 'status':
+                text = self.clean_text(notification.status.content)
+                text_notification += f" posted: {text}"
+        return text_notification
+
     def on_notification(self, notification):
         print(f"[Mastodon] Notification: {notification.type} from {notification.account.acct}")
-        text_notification = f"{notification.account.acct} {notification.type}d your status"
-
-        if notification.type == 'mention':
-            try:
-                tree = fromstring(notification.status.content)
-                text = tree.text_content()
-                text_notification += f": {text}"
-            except Exception as e:
-                print(f"Error parsing notification content: {e}")
-
+        text_notification = self.build_notification(notification)
         try:
             self.interface.sendText(text_notification, channelIndex=2)
         except Exception as e:
@@ -118,8 +132,21 @@ class MeshtasticBot:
         interface.sendText("Connected to Mesh!", channelIndex=2)
 
     def setup_commands(self):
+    
         @self.registry.register('help', help_message='List available commands', example='!help')
         def help_command(*args):
+            # If command provided show help for that command
+            if args:
+                command_name = args[0]
+                command = self.registry.commands.get(command_name)
+                if command:
+                    help_text = f"Command: {command_name}\n"
+                    help_text += f"Help: {command['help']}\n"
+                    help_text += f"Example: {command['example']}"
+                    self.interface.sendText(help_text, channelIndex=2)
+                    return
+                else:
+                    self.interface.sendText(f"Command '{command_name}' not found.", channelIndex=2)
             command_list = "Available commands: " + ", ".join(self.registry.commands.keys())
             self.interface.sendText(command_list, channelIndex=2)
 
@@ -136,7 +163,9 @@ class MeshtasticBot:
             instance = args[0]
             auth_url = self.mastodon_client.login(instance)
             self.interface.sendText(f"{auth_url}", channelIndex=2)
+            # Auth_URL might be too long for the mesh sometimes, not sure if there's any better way of handling that considering we only have 230 chars
             self.interface.sendText(f"Enter your OAuth code:", channelIndex=2)
+            # TODO: Not sure if this is the best way to handle this? i.e. is there/should we wait for a response here instead of using a variable state?
             self.awaiting_oauth = True
 
         @self.registry.register('ping', example='!ping')
