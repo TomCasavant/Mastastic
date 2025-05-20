0c114af755ca58cda053e982d85c0bd184cd81c0
Tom Casavant <tfcasavant@gmail.com>
2025-05-20 13:35:39 -0400

    Added mixin for serial interface, using Data Payload constant now

--- interfaces/messaging_interface.py
+++ interfaces/messaging_interface.py
@@ -3,19 +3,26 @@ Interface wrappers to simplify sending large messages
 """
 
 import meshtastic.serial_interface
+from meshtastic.protobuf import mesh_pb2
 
-class SerialMessagingInterface(meshtastic.serial_interface.SerialInterface):
-    def __init__(self, max_bytes=220):
-        super().__init__()
+DATA_PAYLOAD_LEN = mesh_pb2.Constants.DATA_PAYLOAD_LEN
+
+class MessageSplitMixin:
+    def __init__(self, max_bytes=DATA_PAYLOAD_LEN, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.max_bytes = max_bytes
 
     def sendText(self, text, channelIndex=0):
         # TODO: Actually calculate how many bytes are in text (Emojis can be 4?)
-        # TODO: Actually thinking about this now, we should modify __init__ to take a specific number of chars
         for i in range(0, len(text), self.max_bytes):
             chunk = text[i:i + self.max_bytes]
             super().sendText(chunk, channelIndex=channelIndex)
             print(f"[Mesh] Sent: {chunk}")
 
 
-# TODO: Implement other interfaces, or can we simplify this process?
+class SerialMessagingInterface(MessageSplitMixin, meshtastic.serial_interface.SerialInterface):
+    def __init__(self, max_bytes=DATA_PAYLOAD_LEN):
+        super().__init__(max_bytes=max_bytes)
+
+
+# TODO: Bluetooth interface,
